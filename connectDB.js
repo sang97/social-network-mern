053d1e9bd43c3015e170e818e9e0eb32ae516c60
Sang Nguyen <sang97@vt.edu>
2019-10-31 17:17:32 -0400

    add profile api route, use github api to get github repos

--- connectDB.js
+++ connectDB.js
@@ -5,7 +5,12 @@ const connectDB = async () => {
     //connect to mongodb server
     await mongoose.connect(
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-h8j6g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
     );
     console.log('MongoDB Connected');
   } catch (error) {
